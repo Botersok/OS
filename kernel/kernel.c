3c4f6a184c681ed2efc52fe322cb080f5eb0fbf3
Schrijver <d.schrijver@student.tue.nl>
2021-08-23 16:47:05 +0200

    screen.c is working

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,10 +1,10 @@
 unsigned char port_byte_in(unsigned short port);
 void port_byte_out(unsigned short port, unsigned char data);
-int get_screen_offset(int col, int row);
+void print_char(char character, int row, int col, char attribute);
 int get_cursor();
-void set_cursor(int offset);
-int handle_scrolling(int cursor_offset);
-void print_at(char* message, int col, int row);
+int get_screen_offset(int row, int col);
+void set_cursor(int cursor_offset);
+void print_at(char* message, int row, int col);
 void print(char* message);
 void clear_screen();
 
