43a9d94cd6b7ea5947b48f410dfdc0d8dc7e405e
Schrijver <d.schrijver@student.tue.nl>
2021-08-25 18:16:02 +0200

    this

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -6,7 +6,7 @@ void main() {
 	clear_screen();
 	char *boodschap = "Boodschap";
 	char int_string[255];
-	int_to_ascii(6, int_string);
+	int_to_ascii(445, int_string);
 	print_at(int_string, 24, 79);
 	print(boodschap);
 }
