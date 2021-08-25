6feb803578a2b4f79bfe3aee2f4852eacb178f9f
Schrijver <d.schrijver@student.tue.nl>
2021-08-25 18:20:08 +0200

    int to string

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -7,7 +7,7 @@ void main() {
 	char *boodschap = "Boodschap";
 	char int_string[255];
 	int_to_ascii(445, int_string);
-	print_char('B', 24, 78);
+	print_char('B', 24, 78, 0x0f);
 	print_at(int_string, -1, -1);
 	print(boodschap);
 }
